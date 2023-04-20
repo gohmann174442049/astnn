38b53baee66f17a87fc474cb14294e33ca336ed0
Colin Gohmann <99298617+gohmann174442049@users.noreply.github.com>
2023-04-19 23:52:21 -0400

    update script to output possible clone pairs

--- clone/sampleJavaRepos/100000Methods/LabelFunctionsAndOutputTsv.py
+++ clone/sampleJavaRepos/100000Methods/LabelFunctionsAndOutputTsv.py
@@ -1,12 +1,17 @@
 import xml.etree.ElementTree as ET
 import sys
 
+def replace_last(source_string, replace_what, replace_with):
+    head, _sep, tail = source_string.rpartition(replace_what)
+    return head + replace_with + tail
+idArray=[]
 tree= ET.parse("Java_Repos_sample_esc.xml")
 root= tree.getroot()
-id=23677151+1
+id_func=23677151+1
 for i in root.getchildren():
-    i.attrib.update({"id": str(id)})
-    id+=1
+    i.attrib.update({"id": str(id_func)})
+    #idArray.append(id_func)
+    id_func+=1
 
 tsvArray=[]
 for i in root.getchildren():
@@ -24,12 +29,42 @@ with open("sample_funcs_all.tsv", 'w', encoding='latin-1') as outputFile:
         try:
             if func[1].count('\n') >= 10:
                 success+=1
-                outputFile.write(func[0]+'\t'+"\""+ func[1]
-                                 .replace("\"","\"\"").replace('\n',"\n\t"+"\""))
+                idArray.append(int(func[0]))
+                formattedFun=func[1].replace('\n',"\n\t").replace("\"","\"\"")
+                formattedFun=replace_last(formattedFun, '\t', '')
+                outputFile.write(func[0]+'\t'+"\""+ formattedFun+"\"")
                 outputFile.write('\n')
-        except:
+        except Exception as e:
+            #print(e)
             #invalid char present
             failed+=1
             pass
 print(success)
 print(failed)
+
+possiblePairs=[]
+print(len(idArray))
+'''
+for i in range(0, len(idArray)):
+    if i % 10==0:
+        print(i)
+    for j in range(i, len(idArray)):
+        possiblePairs.append([idArray[i], idArray[j]])
+print(len(possiblePairs))
+'''
+import itertools as it
+n= len(idArray)
+pairs=list(it.combinations(range(n),2))
+print(len(pairs))
+print("writing...")
+with open("possiblePairs.csv", 'w') as outputPairs:
+    outputPairs.write("id1,id2,clone")
+    outputPairs.write('\n')
+    index=0
+    for i in pairs:
+        index+=1
+        if index % 100000==0:
+            print(index)
+        pair=list(i)
+        outputPairs.write(str(pair[0])+","+str(pair[1])+",0")
+        outputPairs.write('\n')
