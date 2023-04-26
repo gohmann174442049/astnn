453fe4a6c458ff5c6a378241adf69bf25e07562c
Colin Gohmann <99298617+gohmann174442049@users.noreply.github.com>
2023-04-25 21:55:13 -0400

    modify the pandas data frame generator pkl

--- clone/sampleJavaRepos/100000Methods/ConverPairsToPandasThenPkl.py
+++ clone/sampleJavaRepos/100000Methods/ConverPairsToPandasThenPkl.py
@@ -1,4 +1,7 @@
 import pandas as pd
-
-data= pd.read_csv("mergedClonePairs.csv")
+print("reading in pandas")
+data= pd.read_csv("mergedClonePairs.csv", index_col = [0])
+#data= pd.read_csv("mergedClonePairs.csv",)
+print(data)
+print("write pickle")
 data.to_pickle("possiblePairs_ids_sample.pkl")
