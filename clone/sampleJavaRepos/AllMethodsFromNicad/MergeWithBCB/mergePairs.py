2a4deee5d6e52841d237fff494e1043fb9042e16
Colin Gohmann <99298617+gohmann174442049@users.noreply.github.com>
2023-05-08 17:00:52 -0400

    make mergePairs.py way more efficient

--- clone/sampleJavaRepos/AllMethodsFromNicad/MergeWithBCB/mergePairs.py
+++ clone/sampleJavaRepos/AllMethodsFromNicad/MergeWithBCB/mergePairs.py
@@ -1,59 +1,21 @@
 import csv
 import random
+import itertools
+
 header=None
 nonPairRows=[]
 rows=[]
-print("load training data...")
-with open("mergedBCBOutputNonPairs.csv", 'r') as file:
-    data=csv.reader(file)
-    index = 0
-    for i in data:
-        if index ==0:
-            index+=1
-            header=i
-            continue
-        if index < 100000:
-            rows.append(i)
-        else:
-            nonPairRows.append(i)
-        index +=1
-        
-rows=rows+random.sample(nonPairRows, 325000)
-rows.insert(0, header)
-print("load candidate pairs 150,000 methods...")
-print(len(rows))
-for i in range(0,10):
-    print(rows[i])
-print("...")
-for i in range(1,11):
-    print(rows[-i])
-with open("../possiblePairs.csv", 'r') as file:
-    data=csv.reader(file)
-    index=0
-    if index % 1000000:
-        print(index, len(data))
-    for i in data:
-        if index==0:
-            index+=1
-            continue
-        index+=1
-        rows.append(i)
-print(index-1)
-print(len(rows))
-tuple_list = [tuple(x) for x in rows]
-if len(tuple_list) == len(set(tuple_list)):
-    print("no repeats!")
-else:
-    print("warning: repeats present")
-#for i in rows:
-#    print(i)
-print("writing out file...")
-with open("../mergedClonePairs.csv", 'w') as outputFile:
-    index=0
-    outputFile.write(",".join(rows[0]))
-    outputFile.write('\n')
-    for row in rows[1:]:
-        outputFile.write(str(index)+","+",".join(row[1:]))
-        outputFile.write('\n')
-        index+=1
-        
+with open("mergedBCBOutputNonPairs.csv", 'r') as file1, open("../possiblePairs.csv", 'r') as file2, open("../mergedClonePairs.csv", 'w', newline='') as outputFile:
+    print("read in bcb pairs")
+    bcbRows=itertools.islice(csv.reader(file1), 425000)
+    #print(len(bcbRows))
+    print("read in data...")
+    datarows=csv.reader(file2)
+    #print(len(datarows))
+    next(datarows)
+    print("combining...")
+    combined_reader = itertools.chain(bcbRows, datarows)
+    print("write both out as csv")
+    writer=csv.writer(outputFile)
+    for row in combined_reader:
+        writer.writerow(row)
